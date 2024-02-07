ad3012820b5531a5c624abdabb5d5a96640451ef
MyPhan105 <143052673+MyPhan105@users.noreply.github.com>
2024-02-07 02:35:15 -0600

    Add files via upload
    Added menu options

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -16,6 +16,37 @@ def create_connection (hostname, uid, pwd, dbname):
 
 myconn = create_connection('sp2024db.c7ugmewi8xhh.us-east-1.rds.amazonaws.com', 'admin', 'Gau113114115116!', 'sp2024db')
 mycursor = myconn.cursor(dictionary=True)
-mycursor.execute('select* from users')
-userlist = mycursor.fetchall()
-print(userlist)
+
+
+
+print('MENU')
+print('a - Add game')
+print('o - Output all games in console')
+print('q - Quit')
+
+option = 'Choose an option!'
+while option != 'q':
+    if option == 'a':
+        game_name = input('Name of the game?')
+        player_number = int(input('What is the maxplayers number?'))
+        gameresult = input('What is the result?')
+        game_duration = int(input('What is the duration of the game?'))
+        game_score = int(input('What is the score?'))
+        try:
+            mycursor.execute('insert into boardgame(gamename, maxplayers, result, gameduration, maxscore) values (%s,%s,%s,%s,%s)', (game_name, player_number, gameresult, game_duration, game_score))
+            print("Game added successfully")
+        except Error as e:
+            print("Error is", e)
+        option = 'Choose an option!'
+    elif option == 'o':
+        mycursor.execute('select* from boardgame')
+        gamelist = mycursor.fetchall()
+        print(gamelist)
+    else:
+        print("Wrong option")
+
+
+
+                             
+
+                         
